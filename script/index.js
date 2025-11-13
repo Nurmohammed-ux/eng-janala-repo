e1279e48e8a33dedad121d6b97c9403c5d970e88
Nurmohammed-ux <hridoynmh@gmail.com>
2025-11-13 23:40:20 +0600

    add some new code

--- script/index.js
+++ script/index.js
@@ -8,11 +8,65 @@ const loadLessons = () => {
     })
 }
 
+const removeActive = () => {
+    const lessonButtons = document.querySelectorAll(".lesson-btn");
+    lessonButtons.forEach(btn => btn.classList.remove("active"));
+}
+
 const loadLevelWord = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
     fetch(url)
     .then(response => response.json())
-    .then(data => displayLevelWord(data.data))
+    .then(data => {
+        removeActive();
+        const clickBtn = document.getElementById(`lesson-btn-${id}`);
+        clickBtn.classList.add("active")
+        displayLevelWord(data.data);
+
+    })
+}
+
+// {
+//     "word": "Cautious",
+//     "meaning": "সতর্ক",
+//     "pronunciation": "কশাস",
+//     "level": 2,
+//     "sentence": "Be cautious while crossing the road.",
+//     "points": 2,
+//     "partsOfSpeech": "adjective",
+//     "synonyms": [
+//         "careful",
+//         "alert",
+//         "watchful"
+//     ],
+//     "id": 3
+// }
+
+const loadWordDetail = async (id) => {
+    const url= `https://openapi.programming-hero.com/api/word/${id}`;
+    const response = await fetch(url);
+    const details = await response.json();
+    displayWordDetails(details.data);
+}
+
+const displayWordDetails = (word) => {
+    console.log(word);
+    const detailsBox = document.getElementById("details-container");
+    detailsBox.innerHTML = `
+                            <div class="bg-[#EDF7FF40] border-3 border-[#EDF7FF] p-6 rounded-3xl">
+        <h2 class="text-4xl font-semibold mb-7">${word.word} (<i class="fa-solid fa-microphone-lines"></i>: <span class="font-bangla">${word.pronunciation}</span>)</h2>
+        <p class="text-2xl font-semibold mb-2.5">Meaning</p>
+        <p class="font-bangla text-2xl font-medium mb-8">${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"}</p>
+        <h2 class="text-2xl font-semibold mb-2">Example</h2>
+        <p class="text-2xl font-normal text-gray-800 mb-8">${word.sentence}</p>
+        <p class="font-bangla text-2xl font-medium mb-2">সমার্থক শব্দ গুলো</p>
+          <span class="btn bg-[#EDF7FF] text-xl font-normal mr-3 px-5 py-1.5 rounded-lg mb-5">${word.synonyms[0] ? word.synonyms[0] : "সমার্থক শব্দ পাওয়া যায়নি"}</span>
+          <span class="btn bg-[#EDF7FF] text-xl font-normal mr-3 px-5 py-1.5 rounded-lg mb-5">${word.synonyms[1] ? word.synonyms[1] : "সমার্থক শব্দ পাওয়া যায়নি"}</span>
+          <span class="btn bg-[#EDF7FF] text-xl font-normal px-5 py-1.5 rounded-lg mb-5">${word.synonyms[2] ? word.synonyms[2] : "সমার্থক শব্দ পাওয়া যায়নি"}</span>
+      </div>
+    
+    `;
+    document.getElementById("word_modal").showModal();
 }
 
 const displayLevelWord = (words) => {
@@ -45,7 +99,7 @@ const displayLevelWord = (words) => {
             <p class="text-xl font-medium mb-4">Meaning / Pronunciation</p>
             <h2 class="font-bangla text-3xl font-semibold text-gray-600 mb-14">${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায়নি"}</h2>
             <div class="flex justify-between items-center">
-              <button class="btn border-none bg-[#1A91FF1A] hover:bg-[#1A91FF59] rounded-sm"><i class="fa-solid fa-circle-info"></i></button>
+              <button  onclick="loadWordDetail(${word.id})" class="btn border-none bg-[#1A91FF1A] hover:bg-[#1A91FF59] rounded-sm"><i class="fa-solid fa-circle-info"></i></button>
               <button class="btn border-none bg-[#1A91FF19] hover:bg-[#1A91FF59]  rounded-sm"><i class="fa-solid fa-volume-high"></i></button>
             </div>
           </div>
@@ -65,7 +119,7 @@ const displayLessons = (lessons) => {
         console.log(lesson);
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-                    <button onclick = "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary text-sm font-semibold">
+                    <button id="lesson-btn-${lesson.level_no}" onclick = "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary text-sm font-semibold lesson-btn">
                     <i class="fa-solid fa-book-open"></i>
                     Lesson-${lesson.level_no}</button>
         `
